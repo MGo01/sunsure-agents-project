9d603a503893db7edb612765f3cc061004fd0dc8
Matthew Gomez <matthew.gomez86@gmail.com>
2021-09-07 22:29:51 -0400

    Resolved database connection issue

--- API/db_conn.php
+++ API/db_conn.php
@@ -4,6 +4,7 @@ header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
     // Fetch credentials from file residing on the server.
     $conf = parse_ini_file('/var/www/db_conn.config');
+    echo($conf);
 
     if ($conf)
     {
