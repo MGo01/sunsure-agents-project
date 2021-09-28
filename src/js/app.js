a94bc67b1504c6fd5a6b87d705e7ebfaada8a0f3
Matthew Gomez <matthew.gomez86@gmail.com>
2021-09-28 11:47:40 -0400

    Trying to fix function scope issue

--- src/js/app.js
+++ src/js/app.js
@@ -90,8 +90,8 @@ function displayClientsTable()
 		console.log("Failure in displayTable()");
 	}
 
-	let results = JSON.parse(xhr.responseText);
-	let clientList = results['results'];
+	var results = JSON.parse(xhr.responseText);
+	var clientList = results['results'];
 	console.log(clientList);
 
 	// For each contact in the JSON array, the contact's
@@ -252,65 +252,65 @@ function searchContacts()
   }
 }
 
-// Creates a client for a specific user and stores it
-// accordingly in the database.
-function createPolicyHolder()
-{
-	let clientFirstName = document.getElementById("clientFirstName").value;
-	let clientLastName = document.getElementById("clientLastName").value;
+// // Creates a client for a specific user and stores it
+// // accordingly in the database.
+// function createPolicyHolder()
+// {
+// 	let clientFirstName = document.getElementById("clientFirstName").value;
+// 	let clientLastName = document.getElementById("clientLastName").value;
 
-	let clientDateOfBirth = document.getElementById("contactEmail").value;
-	let clientSSN = document.getElementById("clientSSN").value;
+// 	let clientDateOfBirth = document.getElementById("contactEmail").value;
+// 	let clientSSN = document.getElementById("clientSSN").value;
 
-	let clientPhone = document.getElementById("clientPhone").value;
-	let clientAddress = document.getElementById("clientAddress").value;
+// 	let clientPhone = document.getElementById("clientPhone").value;
+// 	let clientAddress = document.getElementById("clientAddress").value;
 
-	let clientSecondLineAddress = document.getElementById("clientSecondLineAddress").value;
+// 	let clientSecondLineAddress = document.getElementById("clientSecondLineAddress").value;
 
-	let clientCity = document.getElementById("clientCity").value;
-	let clientAddress = document.getElementById("clientAddress").value;
+// 	let clientCity = document.getElementById("clientCity").value;
+// 	let clientAddress = document.getElementById("clientAddress").value;
 
-	let clientZIP = document.getElementById("clientZIP").value;
-	let clientState = document.getElementById("statesMenu").value;
+// 	let clientZIP = document.getElementById("clientZIP").value;
+// 	let clientState = document.getElementById("statesMenu").value;
 
-	let clientEmail = document.getElementById("clientEmail").value;
-	let clientNumOfDependents = document.getElementById("clientNumOfDependents").value;
+// 	let clientEmail = document.getElementById("clientEmail").value;
+// 	let clientNumOfDependents = document.getElementById("clientNumOfDependents").value;
 
-	let clientNumOfLives = document.getElementById("clientNumOfLives").value;
-	let clientSource = document.getElementById("sourceMenu").value;
+// 	let clientNumOfLives = document.getElementById("clientNumOfLives").value;
+// 	let clientSource = document.getElementById("sourceMenu").value;
 
-	// Remove any special characters in order to ensure all
-	// numbers are a 10 digit string.
-	clientPhone = clientPhone.replace(/[^\w\s]/gi, '');
+// 	// Remove any special characters in order to ensure all
+// 	// numbers are a 10 digit string.
+// 	clientPhone = clientPhone.replace(/[^\w\s]/gi, '');
 
-	// This helps to ensure that none of the form
-	// inputs are left blank and only have alphabetical characters.
-	if (!checkFormNames(clientFirstName, clientLastName))
-		return;
+// 	// This helps to ensure that none of the form
+// 	// inputs are left blank and only have alphabetical characters.
+// 	if (!checkFormNames(clientFirstName, clientLastName))
+// 		return;
 
-	document.getElementById("createClientResult").innerHTML = "";
+// 	document.getElementById("createClientResult").innerHTML = "";
 
-	// Package a JSON payload to deliver to the server that contains all
-	// the contact details in order create the contact.
-  var jsonPayload =
-  	'{"AgentID" : "' + userID + '", "FirstName" : "' + clientFirstName + '", "LastName" : "' + clientLastName + '", "DateOfBirth" : "' + clientDateOfBirth + '", "SSN" : "' + clientSSN + '", "Phone" : "' + clientPhone + '", "Address" : "' + clientAddress + '", "Second_Line_Address" : "' + clientSecondLineAddress + '", "City" : "' + clientCity + '", "ZipCode" : "' + clientZIP + '", "State" : "' + clientState + '", "Email" : "' + clientEmail + '", "NumOfLives" : "' + clientNumOfLives + '", "NumOfDependents" : "' + clientNumOfDependents + '", "PolicyInfoID" : "' + userID + '",  "Source" : "' + clientSource + '"}';
-	var url = urlBase + '/createPolicyHolder.' + extension;
-	var xhr = new XMLHttpRequest();
+// 	// Package a JSON payload to deliver to the server that contains all
+// 	// the contact details in order create the contact.
+//   var jsonPayload =
+//   	'{"AgentID" : "' + userID + '", "FirstName" : "' + clientFirstName + '", "LastName" : "' + clientLastName + '", "DateOfBirth" : "' + clientDateOfBirth + '", "SSN" : "' + clientSSN + '", "Phone" : "' + clientPhone + '", "Address" : "' + clientAddress + '", "Second_Line_Address" : "' + clientSecondLineAddress + '", "City" : "' + clientCity + '", "ZipCode" : "' + clientZIP + '", "State" : "' + clientState + '", "Email" : "' + clientEmail + '", "NumOfLives" : "' + clientNumOfLives + '", "NumOfDependents" : "' + clientNumOfDependents + '", "PolicyInfoID" : "' + userID + '",  "Source" : "' + clientSource + '"}';
+// 	var url = urlBase + '/createPolicyHolder.' + extension;
+// 	var xhr = new XMLHttpRequest();
 
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-	// Basic try and catch to ensure that any server code errors are
-	// handled properly.
-	try
-	{
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("createClientResult").innerHTML = err.message;
-	}
-}
+// 	// Basic try and catch to ensure that any server code errors are
+// 	// handled properly.
+// 	try
+// 	{
+// 		xhr.send(jsonPayload);
+// 	}
+// 	catch(err)
+// 	{
+// 		document.getElementById("createClientResult").innerHTML = err.message;
+// 	}
+// }
 
 // Nifty function that allows for the 'show password'
 // button to function by changing the document element type.
