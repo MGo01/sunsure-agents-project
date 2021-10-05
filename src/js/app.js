26ef8534c68b52e82ca1e617fc2e050c9be2d11a
Matthew Gomez <matthew.gomez86@gmail.com>
2021-10-05 11:27:24 -0400

    Adding/testing insertDependents() to createPolicyHolder()

--- src/js/app.js
+++ src/js/app.js
@@ -232,7 +232,7 @@ function createPolicyHolder()
 				{
 					console.log(endpointmsg);
 					globalPolicyID = jsonObject.ID;
-          // insertDependents(globalPolicyID);  
+          insertDependents(globalPolicyID);  
 				}
 			}
 		};
@@ -293,17 +293,17 @@ function insertDependents(policyID)
 	{
 		// Default options are marked with *
 		const response = await fetch(url, {
-			method: 'POST', // *GET, POST, PUT, DELETE, etc.
-			mode: 'cors', // no-cors, *cors, same-origin
-			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: 'same-origin', // include, *same-origin, omit
+			method: 'POST',
+			mode: 'cors', 
+			cache: 'no-cache', 
+			credentials: 'same-origin', 
 			headers: {
 				'Content-Type': 'application/json'
-				// 'Content-Type': 'application/x-www-form-urlencoded',
+				
 			},
-			redirect: 'follow', // manual, *follow, error
-			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-			body: data // body data type must match "Content-Type" header
+			redirect: 'follow', 
+			referrerPolicy: 'no-referrer', 
+			body: data
 		});
 
 		return response.json(); // parses JSON response into native JavaScript objects
@@ -316,7 +316,7 @@ function insertDependents(policyID)
 		promiseArray.push(postData)
 
 	Promise.all(promiseArray)
-	.then(values=>values.map(value=>console.log(value.url+" ==> "+value.status)))
+	.then(values => values.map(value => console.log(value.url + " ==> " + value.status)))
 	.catch(err=>console.log(err))
 
 	console.log(dependentsArray);
